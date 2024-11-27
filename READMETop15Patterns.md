54ddfbe532671e9ac8844893d802324bab1c57d1
Jon.Olivet <jon.olivet@publix.com>
2024-11-27 10:24:38 -0500

    Implemented the Breadth First Search Pattern.

--- READMETop15Patterns.md
+++ READMETop15Patterns.md
@@ -806,3 +806,51 @@ Various Leetcode playgrounds:
  Example 3:
  Input: numCourses = 1, prerequisites = []
  Output: [0]
+
+# Breadth First Search Pattern (BFS)
+
+ ## 994. Rotting Oranges
+ https://leetcode.com/problems/rotting-oranges/description/
+
+ You are given an m x n grid where each cell can have one of three values:
+
+ 0 representing an empty cell,
+ 1 representing a fresh orange, or
+ 2 representing a rotten orange.
+ Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+ Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+ Example 1:
+ Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+ Output: 4
+
+ Example 2:
+ Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+ Output: -1
+ Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+ Example 3:
+ Input: grid = [[0,2]]
+ Output: 0
+ Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+ ## 127. Word Ladder
+ https://leetcode.com/problems/word-ladder/description/
+
+ A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+ Every adjacent pair of words differs by a single letter.
+ Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+ sk == endWord
+ Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+ Example 1:
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ Output: 5
+ Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+ Example 2:
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ Output: 0
+ Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
---
+++ Top15Patterns.playground/Pages/127WordLadder.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+/*
+ ## 127. Word Ladder
+ https://leetcode.com/problems/word-ladder/description/
+
+ A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+ Every adjacent pair of words differs by a single letter.
+ Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+ sk == endWord
+ Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+ Example 1:
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ Output: 5
+ Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+ Example 2:
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ Output: 0
+ Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ */
+
+func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+    if !wordList.contains(endWord) { return 0 }
+    var wordList = wordList
+    wordList.append(beginWord)
+
+    var adj = [String: [String]]()
+
+    for word in wordList {
+        let word = Array(word)
+        for j in 0..<word.count {
+            let pattern: String = word[..<j] + "*" + word[(j + 1)...]
+            adj[pattern, default: []].append(String(word))
+        }
+    }
+    var visit: Set<String> = [beginWord]
+    var queue = [beginWord]
+    var result = 1
+
+    while !queue.isEmpty {
+        for i in 0..<queue.count {
+            let word = queue.removeFirst()
+            if word == endWord {
+                return result
+            }
+            let wordArray = Array(word)
+            for j in 0..<wordArray.count {
+                let pattern: String = wordArray[..<j] + "*" + wordArray[(j + 1)...]
+                for neiWord in adj[pattern] ?? [] {
+                    if !visit.contains(neiWord) {
+                        visit.insert(neiWord)
+                        queue.append(neiWord)
+                    }
+                }
+            }
+        }
+        result += 1
+    }
+    return 0
+}
+
+let result = ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]) // 5
+print(result)
+
+let result1 = ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]) // 0
+print(result1)
