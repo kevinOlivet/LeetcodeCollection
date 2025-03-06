6a199863252ef8680299d1bf5616810c040fe6a3
Jon.Olivet <jon.olivet@publix.com>
2025-03-06 08:50:26 -0500

    Reverse Nodes in k-group

--- Neetcode150.playground/Pages/146LRUCache.xcplaygroundpage/Contents.swift
+++ Neetcode150.playground/Pages/146LRUCache.xcplaygroundpage/Contents.swift
@@ -34,43 +34,48 @@ import Foundation
  */
 
 class LRUCache {
-
     private var dict: [Int: Int]
     private var order: [Int]
-    private let capacity: Int
-
+    private let cap: Int
     init(_ capacity: Int) {
         dict = [:]
         order = []
-        self.capacity = capacity
+        cap = capacity
     }
-
-    func get(_ key: Int) -> Int {
-        if let value = dict[key] {
-            moveToEnd(key)
-            return value
-        }
-        return -1
-
-    }
-
-    func put(_ key: Int, _ value: Int) {
-        if dict[key] != nil {
-            moveToEnd(key)
-        } else {
-            if order.count == capacity {
-                let tempKey = order.removeFirst()
-                dict[tempKey] = nil
-            }
-            order.append(key)
-        }
-        dict[key] = value
-    }
-
     private func moveToEnd(_ key: Int) {
         if let last = order.last, last != key {
             order.remove(at: order.firstIndex(of: key)!)
             order.append(key)
         }
     }
+    func get(_ key: Int) -> Int {
+        if let value = dict[key] {
+            moveToEnd(key)
+            return value
+        }
+        return -1
+    }
+    func put(_ key: Int, _ value: Int) {
+        if dict[key] != nil {
+            moveToEnd(key)
+        } else {
+            if order.count == cap {
+                let oldest = order.removeFirst()
+                dict[oldest] = nil
+            }
+            order.append(key)
+        }
+        dict[key] = value
+    }
 }
+
+let lRUCache = LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
---
+++ Neetcode150.playground/Pages/25ReverseNodesInKGroup.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+/*
+ ## 25. Reverse Nodes in k-Group
+ https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+
+ Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+ k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+ You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ Example 1:
+ Input: head = [1,2,3,4,5], k = 2
+ Output: [2,1,4,3,5]
+
+ Example 2:
+ Input: head = [1,2,3,4,5], k = 3
+ Output: [3,2,1,4,5]
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+func getKth(curr: ListNode?, k: Int) -> ListNode? {
+    var k = k
+    var curr = curr
+    while curr != nil && k > 0 {
+        curr = curr!.next
+        k -= 1
+    }
+    return curr
+}
+
+func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+    let dummy = ListNode(0, head)
+    var groupPrev = dummy
+    while let kth = getKth(curr: groupPrev, k: k) {
+        let groupNext = kth.next
+        // reverse group
+        var prev = kth.next
+        var curr = groupPrev.next
+        while curr !== groupNext {
+            let next = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = next
+        }
+        let next = groupPrev.next!
+        groupPrev.next = kth
+        groupPrev = next
+    }
+    return dummy.next
+}
+
